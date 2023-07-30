b4af7d52b30ede1f9e9885378f4848ed0547a708
Sunil Park <sunilpark1129@gmail.com>
2023-07-30 04:44:31 -0400

    Added Navbar layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Main from "./page/main/Main";
+import Search from "./page/search/Search";
+import ErrorPage from "./page/error/ErrorPage";
+import InfoPage from "./page/info/InfoPage";
+import MovieModal from "./components/MovieModal";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <div className="wrapper">
+          <Routes>
+            <Route exact path="/" element={<Main />} />
+            <Route path="/search" element={<Search />}>
+              <Route path=":id" element={<MovieModal />} />
+            </Route>
+            <Route path="/info" element={<InfoPage />} />
+            <Route path="/*" element={<ErrorPage />} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
