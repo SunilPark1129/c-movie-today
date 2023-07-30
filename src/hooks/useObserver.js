1739de8edafed0b20fe1de911cd37f4b3fac39db
Sunil Park <sunilpark1129@gmail.com>
2023-07-30 18:19:31 -0400

    Added Observer

---
+++ src/hooks/useObserver.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect, useMemo } from "react";
+
+export default function useObserver(ref, option) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  function cb(entries) {
+    const [entry] = entries;
+    setIsVisible(entry.isIntersecting);
+  }
+
+  const optionMemo = useMemo(() => {
+    return option;
+  }, [option]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(cb, optionMemo);
+    const target = ref.current;
+    if (target && !isVisible) {
+      observer.observe(target);
+    }
+
+    return () => observer.unobserve(target);
+  }, [ref, optionMemo]);
+
+  return isVisible;
+}
