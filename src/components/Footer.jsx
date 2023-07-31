cad5ed9d8655be7683d1f92e520a8cd6037d5fa6
Sunil Park <sunilpark1129@gmail.com>
2023-07-31 00:44:20 -0400

    Added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <p>Cinema Movie</p>
+      <p>
+        Cinema Movie (CM) is a site that provides users with a convenient way to
+        view movie reviews. We extracted movie information with API and
+        established it to make it easy for users to find the movie information.
+        We run CM for the purpose of providing movie reviews, so we provide
+        users with ratings, release dates, and overviews of movies.
+      </p>
+      <p>© COPYRIGHT CINEMA MOVIE SEWELL NJ - 2022</p>
+    </footer>
+  );
 }
