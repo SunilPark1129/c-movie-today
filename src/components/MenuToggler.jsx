c03db2020eedb246dece0e2f7345d5fa441fc9ab
Sunil Park <sunilpark1129@gmail.com>
2023-08-02 13:07:41 -0400

    Fixed overflow auto and hidden

--- src/components/MenuToggler.jsx
+++ src/components/MenuToggler.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect } from "react";
 
+import { useSelected } from "../hooks/useReducer";
+
 export default function MenuToggler({ menuOpen, setMenuOpen }) {
+  const { selectedMovie } = useSelected();
+
   useEffect(() => {
     if (menuOpen) {
       document.querySelector("body").style.overflow = "hidden";
@@ -12,7 +16,7 @@ export default function MenuToggler({ menuOpen, setMenuOpen }) {
 
   function handleResize() {
     const windowWidth = window.innerWidth;
-    if (windowWidth >= 1000) {
+    if (windowWidth >= 1000 && !selectedMovie) {
       setMenuOpen(false);
       document.querySelector("body").style.overflow = "auto";
     }
@@ -23,7 +27,7 @@ export default function MenuToggler({ menuOpen, setMenuOpen }) {
     return () => {
       window.removeEventListener("resize", handleResize);
     };
-  }, []);
+  }, [selectedMovie]);
 
   return (
     <div className="toggler" onClick={() => setMenuOpen((prev) => !prev)}>
